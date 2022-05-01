59ead927bde6976722509a893b6b6c3a0789fb52
mroy35034 <mroy35034@gmail.com>
2022-05-01 08:14:16 +0600

    setup server and added get and post request

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
 const express = require('express');
-require('dotenv').config();
 const cors = require('cors');
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Using Middleware
 app.use(cors());
 app.use(express.json());
-const ObjectId = require('mongodb').ObjectId;
+
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PWD}@cluster1.vfksi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,10 +20,26 @@ async function run() {
       await client.connect();
       const productCollection = client.db("productInventory").collection("product");
 
+      // Get Request for all product
+      app.get('/product', async (req, res) => {
+         const query = {};
+         const cursor = productCollection.find(query);
+         const result = await cursor.toArray();
+         res.send(result);
+      });
+
+       // Get Request for one particular product
+       app.get('/product/:id', async (req, res) => {
+          const id = req.params.id;
+         const query = {_id: ObjectId(id)};
+         const result = await productCollection.findOne(query);
+         res.send(result);
+      });
+
    } finally { }
 }
 run().catch(console.dir());
 
 app.listen(port, () => {
-   console.log('Listening running port is ' + port);
+   console.log('Listening port ' + port);
 });
