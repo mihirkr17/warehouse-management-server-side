af3f5dc55e84d1d76505426aa843a67ce8f599d2
mroy35034 <mroy35034@gmail.com>
2022-05-02 21:40:35 +0600

    added delete functionality for single item

--- index.js
+++ index.js
@@ -67,6 +67,14 @@ async function run() {
          res.send(result);
       });
 
+      // delete item request from database
+      app.delete('/product/:id', async(req, res) => {
+         const id = req.params.id;
+         const query = {_id: ObjectId(id)};
+         const result = await productCollection.deleteOne(query);
+         res.send(result);
+      });
+
    } finally { }
 }
 run().catch(console.dir());
