2e7c2c10ef4149f22600229a2136de2befe5066d
mroy35034 <mroy35034@gmail.com>
2022-05-07 09:31:10 +0600

    added heroku

--- index.js
+++ index.js
@@ -120,6 +120,10 @@ async function run() {
 }
 run().catch(console.dir());
 
+app.get('/', (req, res) => {
+   res.send("Running Warehouse Services...");
+});
+
 app.listen(port, () => {
    console.log('Listening port ' + port);
 });
